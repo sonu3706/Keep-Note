b61f9c86acde4123a4f936f05d16086938cf2974
Chandan Mishra <chandankumar.mishra@itcinfotech.com>
2020-04-03 03:18:53 +0530

    registry service added

---
+++ keepnote-registry/src/main/java/com/keepnote/registry/KeepnoteRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.keepnote.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepnoteRegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeepnoteRegistryApplication.class, args);
+    }
+
+}
