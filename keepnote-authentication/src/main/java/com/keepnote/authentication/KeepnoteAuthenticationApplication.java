064772b02b9e26c3a9801dbc479dd032d7740096
Chandan Mishra <chandankumar.mishra@itcinfotech.com>
2020-04-03 03:13:35 +0530

    Authentication service added

---
+++ keepnote-authentication/src/main/java/com/keepnote/authentication/KeepnoteAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.keepnote.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepnoteAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeepnoteAuthenticationApplication.class, args);
+    }
+
+}
---
+++ keepnote-authentication/src/test/java/com/keepnote/authentication/KeepnoteAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keepnote.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeepnoteAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
