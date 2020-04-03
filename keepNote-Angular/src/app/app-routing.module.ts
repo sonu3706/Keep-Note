e0e9415fc86aad850d0d5d61966c6c8a465f5839
Chandan Mishra <chandankumar.mishra@itcinfotech.com>
2020-04-03 10:25:08 +0530

    login, register, navbar components created

--- keepNote-Angular/src/app/app-routing.module.ts
+++ keepNote-Angular/src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./modules/authentication/authentication.module').then(
+        auth => auth.AuthenticationModule
+      )
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
