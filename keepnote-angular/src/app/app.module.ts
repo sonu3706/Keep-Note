61a8f29714dbb885bc87acfac640ed2a197b4066
Chandan Mishra <chandankumar.mishra@itcinfotech.com>
2020-04-11 18:52:13 +0530

    code optimized

--- keepnote-angular/src/app/app.module.ts
+++ keepnote-angular/src/app/app.module.ts
@@ -4,19 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
+import {SharedModule} from "./modules/shared/shared.module";
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    HeaderComponent,
-    FooterComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- keepnote-angular/src/app/modules/shared/shared.module.ts
+++ keepnote-angular/src/app/modules/shared/shared.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {HeaderComponent} from "./components/header/header.component";
+import {FooterComponent} from "./components/footer/footer.component";
+import {RouterModule} from "@angular/router";
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent, FooterComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,RouterModule
+  ],
+  exports: [HeaderComponent,FooterComponent]
+
 })
 export class SharedModule { }
